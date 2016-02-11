67f32746c1adb30af3365652973c542ea600a7ee
Stephen Wetzel <stephen.wetzel1@gmail.com>
2016-02-11 15:57:55 -0500

    get symbols from db, and store tweets in db

--- parseTweets.py
+++ parseTweets.py
@@ -7,7 +7,22 @@ symbols = ["AAPL", "SPY", "IBM"]
 baseUrl = "https://api.stocktwits.com/api/2/streams/symbol/"
 urlSuffix = ".json"
 
-for symbol in symbols:
+con = mdb.connect('localhost', 'dbuser', 'dbuser', 'mydb');
+with con: #this creates the two tables, symbols and prices, and drops them if they already exist
+	cur = con.cursor()
+	sql = "SELECT * FROM Symbols WHERE type='index'"
+	cur.execute(sql)
+	symbols = cur.fetchall()
+
+
+for row in symbols:
+	exchange = row[0]
+	symbol = row[1]
+	futureType = row[2]
+	futureCat  = row[3]
+	expireMonth = row[4]
+	expireYear = row[5]
+	stillUpdated = row[6]
 	url = baseUrl + symbol + urlSuffix
 	#https://api.stocktwits.com/api/2/streams/symbol/AAPL.json
 	
@@ -15,8 +30,22 @@ for symbol in symbols:
 	data = json.loads(data)
 	
 	for tweet in data['messages']:
-		print("Message ID: " + str(tweet['id']))
-		print("Message body: " + str(tweet['body']))
-		print("Message Time: " + str(tweet['created_at']))
-		print("Message Username: " + str(tweet['user']['username']))
+		messageId = tweet['id']
+		body = tweet['body'].encode('utf-8')
+		timestamp = tweet['created_at'].encode('utf-8')
+		username = tweet['user']['username'].encode('utf-8')
+		
+		
+		print("Message ID: " + str(messageId))
+		print("Message body: " + str(body))
+		print("Message Time: " + str(timestamp))
+		print("Message Username: " + str(username))
 		print "\n"
+		
+		sql = "INSERT INTO Tweets(timestamp, message_id, body, username) VALUES ('%s', '%s', '%s', '%s') ON DUPLICATE KEY UPDATE timestamp=VALUES(timestamp), message_id=VALUES(message_id), body=VALUES(body), username=VALUES(username)" % \
+		(timestamp, messageId, body, username)
+		#print sql
+		cur.execute(sql)
+	con.commit()
+		
+		
